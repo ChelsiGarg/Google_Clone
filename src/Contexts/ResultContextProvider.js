27c1e7d220b2c2da2c240a1fa08adcd0092a7243
chelsi <chelsigarg178@gmail.com>
2022-08-15 15:54:49 +0530

    first commit

---
+++ src/Contexts/ResultContextProvider.js
@@ -0,0 +1,37 @@
+import React, {createContext, useContext, useState} from 'react';
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({children}) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const getResults = async(type) => {
+        setIsLoading(true);
+
+        const response = await fetch (`${baseUrl}${type}`, {
+            method: 'GET',
+            headers: {
+                'x-user-agent': 'desktop',
+                'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                'x-rapidapi-key': '2b8bcf9384mshe27b0afd1ae480fp1edf07jsnb129461cf1ac',
+            }
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+        setResults(data);
+        setIsLoading(false);
+    }
+
+    return(
+        <ResultContext.Provider value={{getResults, results, searchTerm, setSearchTerm, isLoading}}>
+           {children}
+        </ResultContext.Provider>
+    )
+}
+
+export const useResultContext = () => useContext(ResultContext);
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <div className="text-center p-10 mt-10 border-t
+    dark:border-gray-700 border-gray-200 ">
+      © 2021 Giggle, Inc.
+    </div>
+
+    <div className="text-center p-2 mt-2 pb-5
+    dark:border-gray-700 border-gray-200 ">
+      Developed by - Chelsi Garg
+    </div>
+    </>
+  )
+}
